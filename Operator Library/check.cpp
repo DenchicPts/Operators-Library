93f835ec3606e26b9c005f0c4d88d5a005740c42
DenchicPts <92357491+DenchicPts@users.noreply.github.com>
2023-04-07 00:54:25 +0300

    Update check.cpp

--- Operator Library/check.cpp
+++ Operator Library/check.cpp
@@ -1,7 +1,15 @@
 #include "DenchicPts.h"
 
 void main() {
-	
 
+	vector<int> abobus;
+	vector<float> abobus2;
+	VectorFill(abobus, 100, 5);
+	VectorFill(abobus2, 100, 5);
+	cout << abobus + abobus2;
+	
+	set<int> geg;
+	SetFill(geg, 100, 20);
+	cout << geg;
 	return;
 }
