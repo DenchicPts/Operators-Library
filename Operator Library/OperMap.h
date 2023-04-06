4bdf9b17fda645d32b6a46328cf9fcde31efd1a6
DenchicPts <92357491+DenchicPts@users.noreply.github.com>
2023-04-07 00:01:36 +0300

    Map & Multimap
    Added new file

---
+++ Operator Library/OperMap.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "DenchicPts.h"
+#include <map>
+using namespace std;
+
+
+//                                     Operation Work
+
+template<typename Type, typename Type2>
+ostream& operator<<(ostream& stream, const map<Type, Type2>& m) { // Вывод контейнера map через cout
+	for (const auto& i : m)
+		stream << i.first << " : " << i.second << endl;
+	return stream;
+}
+template<typename Type, typename Type2>
+ostream& operator<<(ostream& stream, const multimap<Type, Type2>& m) { // Вывод мульти контейнера map через cout
+	for (const auto& i : m)
+		stream << i.first << " : " << i.second << endl;
+	return stream;
+}
+
+template <typename Type1, typename Type2>
+map<Type1, Type2>
+operator+(const map<Type1, Type2>& map1, const map<Type1, Type2>& map2) {
+	map<Type1, Type2> result = map1;
+	result.insert(map2.begin(), map2.end());
+	return result;
+}
+
+template <typename Type1, typename Type2>
+multimap<Type1, Type2>
+operator+(const multimap<Type1, Type2>& map1, const multimap<Type1, Type2>& map2) {
+	multimap<Type1, Type2> result = map1;
+	result.insert(map2.begin(), map2.end());
+	return result;
+}
+
+//                              Default Function
+
+template<typename Type, typename Type2>
+void MapFill(map<Type, Type2>& m, const size_t& cell) { // заполнение map вводом с клавиатуры
+	Type first;
+	Type2 second;
+	for (size_t i = 0; i < cell; i++)
+	{
+		cout << "Enter key for value : ";
+		cin >> first;
+		cout << "Enter value : ";
+		cin >> second;
+		m.emplace(first, second);
+	}
+}
+
+template<typename Type, typename Type2>
+void MultiMapFill(multimap<Type, Type2>& m, const size_t& cell) { // заполнение multimap вводом с клавиатуры
+	Type first;
+	Type2 second;
+	for (size_t i = 0; i < cell; i++)
+	{
+		cout << "Enter key for value : ";
+		cin >> first;
+		cout << "Enter value : ";
+		cin >> second;
+		m.emplace(first, second);
+	}
+}
+
+template <typename Type1, typename Type2>
+multimap<Type1, Type2>
+MapToMulti(const map<Type1, Type2>& map1, const map<Type1, Type2>& map2)
+{
+	multimap<Type1, Type2> result;
+	result.insert(map1.begin(), map1.end());
+	result.insert(map2.begin(), map2.end());
+	return result;
+}
+
+template <typename Type1, typename Type2>
+multimap<Type1, Type2>
+MapToMulti(multimap<Type1, Type2>& result, const map<Type1, Type2>& map1, const map<Type1, Type2>& map2)
+{
+	result.insert(map1.begin(), map1.end());
+	result.insert(map2.begin(), map2.end());
+	return result;
+}
--- Operator Library/OperSet.h
+++ Operator Library/OperSet.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include "DenchicPts.h"
 #include <set>
 using namespace std;
