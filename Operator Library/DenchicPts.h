a43ab483f0c4f54231bd5ccc292382c95dc2348b
DenchicPts <92357491+DenchicPts@users.noreply.github.com>
2023-04-07 21:30:20 +0300

    Text exercise
    for latvian text

--- Operator Library/DenchicPts.h
+++ Operator Library/DenchicPts.h
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <algorithm>
 #include <sstream>
+#include <fstream>
 #include <iomanip>
 #include <cstdlib>
 #include "OperVector.h"
--- Operator Library/check.cpp
+++ Operator Library/check.cpp
@@ -1,26 +1,47 @@
 #include "DenchicPts.h"
 
+map<string, int> count_words(string const& str, const string& punctutation) {
+
+    map<string, int> word_counts;
+    string word;
+    stringstream stream(str);
+    while (stream >> word) {
+        // Исключаем слова, короче 4 символов
+        if (word.length() < 4) {
+            continue;
+        }
+        // Удаляем знаки препинания из слова
+        for (char const& c : punctutation) {
+            word.erase(remove(word.begin(), word.end(), c), word.end());
+        }
+        // Увеличиваем счетчик для данного слова
+        ++word_counts[word];
+    }
+    return word_counts;
+}
+
 void main() {
+    setlocale(LC_ALL, "lv_LV.UTF-8");
+	string punctutation = ",./\'\";:?&^*(){}[]$#@!<>-=+";
+	const string filename = "text.txt";
+	vector<string> vardi;
+
+	ifstream file(filename);
+	stringstream buffer;
+	buffer << file.rdbuf();
+	string content = buffer.str();
+
+    map<string, int> word_counts = count_words(content, punctutation);
+    vector<pair<string, int>> word_freq(word_counts.begin(), word_counts.end());
+
+    sort(word_freq.begin(), word_freq.end(), [](auto const& p1, auto const& p2) {
+        return p1.second > p2.second;
+        });
 
 
-	multimap<int, string> abobus;
-	abobus.emplace(1, "2");
-	abobus.emplace(3, "4");
-	abobus.emplace(2, "3");
-	abobus.emplace(5, "6");
-	abobus.emplace(6, "7");
-
-
-	map<int, string> abobus2;
-	abobus.emplace(2, "2");
-	abobus.emplace(4, "4");
-	abobus.emplace(3, "3");
-	abobus.emplace(6, "6");
-	abobus.emplace(7, "7");
-
-	multimap<int, string> abobus3 = abobus2 + abobus;
-
-	cout << abobus + abobus2;
-	cout << abobus3;
+    cout << "Vardi:" << endl;
+    for (int i = 0; i < min(5, static_cast<int>(word_freq.size())); ++i) {
+        cout << word_freq[i].first << ": " << word_freq[i].second << endl;
+    }
 	return;
 }
