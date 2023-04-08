dc925a58d901c4694fc8590022550584054186a8
DenchicPts <92357491+DenchicPts@users.noreply.github.com>
2023-04-08 16:44:33 +0300

    Merge branch 'vector'

--- Operator Library/check.cpp
+++ Operator Library/check.cpp
@@ -1,7 +1,5 @@
 #include "DenchicPts.h"
 
-using namespace std;
-
 int main() {
     setlocale(LC_ALL, "lv_LV.UTF-8");
     string punctuation = ",./\'\";:?&^*(){}[]$#@!<>-=+";
@@ -12,22 +10,23 @@ int main() {
         cout << "Не удалось открыть файл" << endl;
         return 0;
     }
-    map<string, int> word_counts = wordCounter(file,punctuation);
+    map<string, int> word_counts = wordCounter(file, punctuation);
     file.close();
 
-    vector<pair<string, int>> word_freq(word_counts.begin(), word_counts.end());
+    vector<pair<string, int>> word_freq(word_counts.begin(), word_counts.end()); //
+
     sort(word_freq.begin(), word_freq.end(), [](auto const& p1, auto const& p2) {
-        return p1.second > p2.second;
+        return p1.second > p2.second; // лямбда функция не обязательно, можно написать просто функцию
         });
 
     cout << "Top 5 words" << endl;
     for (int i = 0; i < 5 && i < word_freq.size(); ++i) {
-        cout << word_freq[i].first << ": " << word_freq[i].second << endl;
+        cout << word_freq[i].first << ": " << word_freq[i].second << endl; // Выводим ассоциативный массив
     }
     return 0;
 }
 
-map<string, int> wordCounter(ifstream& file,string& punctuation)
+map<string, int> wordCounter(ifstream& file, string& punctuation)
 {
     map<string, int> word_counts;
     string word;
@@ -39,6 +38,8 @@ map<string, int> wordCounter(ifstream& file,string& punctuation)
         // Удаляем знаки препинания из слова
         for (char const& c : punctuation) {
             word.erase(remove(word.begin(), word.end(), c), word.end());
+            // Удаляем символ c из строки word с помощью функции remove
+            // и затем удаляем символы, которые были перемещены в конец диапазона, с помощью метода erase
         }
         // Увеличиваем счетчик для данного слова
         ++word_counts[word];
